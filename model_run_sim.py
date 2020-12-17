4d634fc20079d8ad0dbec90535530f138d44de1c
Oleksandr Slovak <slovak194@gmail.com>
2020-12-17 13:47:31 +0100

    + simple logs

--- model_run_sim.py
+++ model_run_sim.py
@@ -8,13 +8,7 @@ import numpy as np
 
 import control as ctl
 
-from scipy.integrate import odeint
-
-from model_cart_pendulum import model_sim_lnp, A_sim_lnp, B_sim_lnp, A_ctrl_lnp, B_ctrl_lnp
-
-# model_sim(y0, 0.0, params, inp)
-# t = np.linspace(0, 10, 101)
-# sol = odeint(model_sim, y0, t, args=(params, inp))
+from model_cart_pendulum import model_sim_lnp, A_ctrl_lnp, B_ctrl_lnp
 
 
 class PendulumVis:
@@ -26,10 +20,11 @@ class PendulumVis:
         if event.key == ' ':
             self.running = False
 
-    def update(self, l_state):
+    def update(self, l_state, l_inp):
 
         l_x = l_state[0]
         l_theta = l_state[1]
+        F = l_inp[0]
 
         pend = np.array([np.cos(l_theta + np.pi/2), np.sin(l_theta + np.pi/2)])*1.0
 
@@ -44,11 +39,12 @@ class PendulumVis:
 
             self.cart = self.ax.plot(l_x, 0.0, 'o')
             self.pend = self.ax.plot([l_x, l_x + pend[0]], [0.0, pend[1]], '.-')
+            self.force = self.ax.plot([l_x, l_x + F], [0.0, 0.0], '.-')
 
             self.ax.set_aspect("equal")
 
-            self.ax.set_xlim([-1.5, 1.5])
-            self.ax.set_ylim([-1.5, 1.5])
+            self.ax.set_xlim([-2, 2])
+            self.ax.set_ylim([-0.25, 1.25])
 
             self.ax.grid(True, which="major")
             self.ax.minorticks_on()
@@ -59,6 +55,7 @@ class PendulumVis:
         else:
             self.cart[0].set_data(l_x, 0.0)
             self.pend[0].set_data([l_x, l_x + pend[0]], [0.0, pend[1]])
+            self.force[0].set_data([l_x, l_x + F], [0.0, 0.0])
 
 
 def rk4step(f, y_i, h, args=()):
@@ -72,21 +69,11 @@ def rk4step(f, y_i, h, args=()):
     y_ip1 = y_i + (h / 6.) * (k1 + 2*k2 + 2*k3 + k4)
     return y_ip1
 
-#
-# class KeyCallback:
-#     running = True
-#
-#     def press(self, event):
-#         if event.key == ' ':
-#             self.running = False
-
-
-# kp = KeyCallback()
-
+# %%
 
 y0 = np.array([
     0.0,
-    0.2,
+    30*np.pi/180,
     0.0,
     0.0
 ])
@@ -98,42 +85,61 @@ params = np.array([
     9.81,
 ])
 
-Qx1 = np.diag([1, 1, 1])
+Qx1 = np.diag([1, 50, 1])
 Qu1a = np.diag([1])
 K, X, E = ctl.lqr(A_ctrl_lnp(params), B_ctrl_lnp(params), Qx1, Qu1a)
 
 K = np.array(K).squeeze()
 
-# %%
-
 y = y0
-
-# kp.running = True
 
 pend = PendulumVis()
 
-ts = 0.1
+ts = 0.01
 
-# K = np.array([
-#     0.0,
-#     100.0,
-#     0.0,
-#     50.0,
-# ])
+Y = []
+INP = []
 
 while pend.running:
 
-    # inp = np.array([1.0])
     inp = np.array([-K @ y[1:]])
+
+    Y.append(y)
+    INP.append(inp)
 
     y = rk4step(model_sim_lnp, y, ts, args=(params, inp))
 
-    pend.update(y)
-    # vis_pendulum(y, ax)
+    pend.update(y, inp)
     print(y)
 
     plt.pause(ts)
 
+Y = np.array(Y)
+INP = np.array(INP)
+
+f3 = plt.figure(3)
+f3.clf()
+
+axs = []
+
+for i in range(3):
+    axs.append(f3.add_subplot(3, 1, i+1))
+    axs[-1].grid(True, which="major")
+    axs[-1].minorticks_on()
+    axs[-1].grid(True, which="minor", linestyle=':', linewidth=1)
+
+
+axs[0].plot(Y[:, 0], '.-', label="x")
+axs[0].plot(Y[:, 2], '.-', label="x_dot")
+axs[0].legend()
+
+
+axs[1].plot(Y[:, 1], '.-', label="theta")
+axs[1].plot(Y[:, 3], '.-', label="theta_dot")
+axs[1].legend()
+
+axs[2].plot(INP, '.-', label='F')
+axs[2].legend()
 
 # %%
 #
