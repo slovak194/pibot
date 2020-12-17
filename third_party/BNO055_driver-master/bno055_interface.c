e12c4d80f4b10fd6fc232e2cd928b962129c0c67
Oleksandr Slovak <slovak194@gmail.com>
2020-12-18 00:12:53 +0100

    simple imu abstraction

--- third_party/BNO055_driver-master/bno055_interface.c
+++ third_party/BNO055_driver-master/bno055_interface.c
@@ -5,83 +5,27 @@
 #include <thread>
 #include <chrono>
 
-#include <pigpio.h>
-
 using namespace std::chrono_literals;
 
+int16_t BNO055::m_i2c_handle = -1;
 
-/*----------------------------------------------------------------------------*
- *  struct bno055_t parameters can be accessed by using BNO055
- *  BNO055_t having the following parameters
- *  Bus write function pointer: BNO055_WR_FUNC_PTR
- *  Bus read function pointer: BNO055_RD_FUNC_PTR
- *  Burst read function pointer: BNO055_BRD_FUNC_PTR
- *  Delay function pointer: delay_msec
- *  I2C address: dev_addr
- *  Chip id of the sensor: chip_id
- *---------------------------------------------------------------------------*/
-struct bno055_t bno055;
-
-int16_t _HandleBNO;
-int _i2cChannel = 1;
-
-s32 bno055_suspend(void) {
-
+BNO055::BNO055() {
   s32 comres = BNO055_ERROR;
 
-  u8 power_mode = BNO055_INIT_VALUE;
+  if (gpioInitialise() < 0)
+  {
+    std::cout <<"Initialisation error of the GPIO \n Closing program..."<< std::endl;
+  }
 
-  /*  For de - initializing the BNO sensor it is required
-   * to the operation mode of the sensor as SUSPEND
-   * Suspend mode can set from the register
-   * Page - page0
-   * register - 0x3E
-   * bit positions - 0 and 1*/
-  power_mode = BNO055_POWER_MODE_SUSPEND;
+  m_i2c_handle=i2cOpen(m_i2c_channel, BNO055_I2C_ADDR1,0);
 
-  /* set the power mode as SUSPEND*/
-  comres += bno055_set_power_mode(power_mode);
+  m_sensor_parameters.bus_write = BNO055::BNO055_I2C_bus_write;
+  m_sensor_parameters.bus_read = BNO055::BNO055_I2C_bus_read;
+  m_sensor_parameters.delay_msec = BNO055::BNO055_delay_msek;
+  m_sensor_parameters.dev_addr = BNO055_I2C_ADDR1;
 
-  return comres;
-
-}
-
-
-s32 bno055_init(void) {
-
-  s32 comres = BNO055_ERROR;
-
-  u8 power_mode = BNO055_INIT_VALUE;
-
-  I2C_routine();
-
-  /*--------------------------------------------------------------------------*
-   *  This API used to assign the value/reference of
-   *  the following parameters
-   *  I2C address
-   *  Bus Write
-   *  Bus read
-   *  Chip id
-   *  Page id
-   *  Accel revision id
-   *  Mag revision id
-   *  Gyro revision id
-   *  Boot loader revision id
-   *  Software revision id
-   *-------------------------------------------------------------------------*/
-  comres = bno055_init(&bno055);
-
-  /*  For initializing the BNO sensor it is required to the operation mode
-   * of the sensor as NORMAL
-   * Normal mode can set from the register
-   * Page - page0
-   * register - 0x3E
-   * bit positions - 0 and 1*/
-  power_mode = BNO055_POWER_MODE_NORMAL;
-
-  /* set the power mode as NORMAL*/
-  comres += bno055_set_power_mode(power_mode);
-
+  comres = bno055_init(&m_sensor_parameters);
+  comres += bno055_set_power_mode(BNO055_POWER_MODE_NORMAL);
 
   /************************* START READ RAW FUSION DATA ********
    * For reading fusion data it is required to set the
@@ -100,113 +44,37 @@ s32 bno055_init(void) {
    * based on the user need configure the operation mode*/
   comres += bno055_set_operation_mode(BNO055_OPERATION_MODE_NDOF);
 
-  /*----------------------------------------------------------------*
-   ************************* END INITIALIZATION *************************
-   *-----------------------------------------------------------------*/
-
-  return comres;
 }
 
-#ifdef  BNO055_API
+BNO055::~BNO055() {
 
-/*--------------------------------------------------------------------------*
- *  The following API is used to map the I2C bus read, write, delay and
- *  device address with global structure bno055_t
- *-------------------------------------------------------------------------*/
+  s32 comres = BNO055_ERROR;
 
-/*-------------------------------------------------------------------------*
- *  By using bno055 the following structure parameter can be accessed
- *  Bus write function pointer: BNO055_WR_FUNC_PTR
- *  Bus read function pointer: BNO055_RD_FUNC_PTR
- *  Delay function pointer: delay_msec
- *  I2C address: dev_addr
- *--------------------------------------------------------------------------*/
-s8 I2C_routine(void)
-{
-    if (gpioInitialise() <0)
-    {
-      std::cout <<"Initialisation error of the GPIO \n Closing program..."<< std::endl;
-      return -1;
-    }
+  /*  For de - initializing the BNO sensor it is required
+   * to the operation mode of the sensor as SUSPEND
+   * Suspend mode can set from the register
+   * Page - page0
+   * register - 0x3E
+   * bit positions - 0 and 1*/
 
-    _HandleBNO=i2cOpen(_i2cChannel, BNO055_I2C_ADDR1,0);
+  comres += bno055_set_power_mode(BNO055_POWER_MODE_SUSPEND);
 
-    bno055.bus_write = BNO055_I2C_bus_write;
-    bno055.bus_read = BNO055_I2C_bus_read;
-    bno055.delay_msec = BNO055_delay_msek;
-    bno055.dev_addr = BNO055_I2C_ADDR1;
-
-    return BNO055_INIT_VALUE;
 }
 
-/************** I2C buffer length******/
 
-#define I2C_BUFFER_LEN 8
-#define I2C0           5
+bno055_euler_double_t BNO055::GetEuler() {
+  struct bno055_euler_double_t d_euler_hpr;
+  auto result = bno055_convert_double_euler_hpr_deg(&d_euler_hpr);
+  return d_euler_hpr;
+}
 
-/*-------------------------------------------------------------------*
- *
- *  This is a sample code for read and write the data by using I2C
- *  Use either I2C  based on your need
- *  The device address defined in the bno055.h file
- *
- *--------------------------------------------------------------------*/
 
-/*  \Brief: The API is used as I2C bus write
- *  \Return : Status of the I2C write
- *  \param dev_addr : The device address of the sensor
- *  \param reg_addr : Address of the first register,
- *   will data is going to be written
- *  \param reg_data : It is a value hold in the array,
- *      will be used for write the value into the register
- *  \param cnt : The no of byte of data to be write
- */
-s8 BNO055_I2C_bus_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
+s8 BNO055::BNO055_I2C_bus_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
 {
 
-//    printf("[WR] ");
-//    printf("dev_addr: 0x%02x | ", dev_addr);
-//    printf("reg_addr: 0x%02x | ", reg_addr);
-//    printf("cnt: 0x%02x | ", cnt);
-//    printf("reg_data: ");
-//
-//    for (int i = 0; i < cnt; i++) {
-//      printf("0x%02x ", *(reg_data + i));
-//    }
-//
-//    printf("\n");
+  int result = i2cWriteI2CBlockData(m_i2c_handle, static_cast<unsigned>(reg_addr), static_cast<char *>(static_cast<void *>(reg_data)), static_cast<unsigned>(cnt));
 
-    int result = i2cWriteI2CBlockData(_HandleBNO, static_cast<unsigned>(reg_addr), static_cast<char *>(static_cast<void *>(reg_data)), static_cast<unsigned>(cnt));
-
-    s32 BNO055_iERROR = BNO055_INIT_VALUE;
-
-//    u8 array[I2C_BUFFER_LEN];
-//    u8 stringpos = BNO055_INIT_VALUE;
-
-//    array[BNO055_INIT_VALUE] = reg_addr;
-//    for (stringpos = BNO055_INIT_VALUE; stringpos < cnt; stringpos++)
-//    {
-//        array[stringpos + BNO055_I2C_BUS_WRITE_ARRAY_INDEX] = *(reg_data + stringpos);
-//    }
-//}
-
-/*
- * Please take the below APIs as your reference for
- * write the data using I2C communication
- * "BNO055_iERROR = I2C_WRITE_STRING(DEV_ADDR, ARRAY, CNT+1)"
- * add your I2C write APIs here
- * BNO055_iERROR is an return value of I2C read API
- * Please select your valid return value
- * In the driver BNO055_SUCCESS defined as 0
- * and FAILURE defined as -1
- * Note :
- * This is a full duplex operation,
- * The first read data is discarded, for that extra write operation
- * have to be initiated. For that cnt+1 operation done
- * in the I2C write string function
- * For more information please refer data sheet SPI communication:
- */
-
+  s32 BNO055_iERROR = BNO055_INIT_VALUE;
 
   if (result == 0) {
     BNO055_iERROR = BNO055_SUCCESS;
@@ -214,84 +82,30 @@ s8 BNO055_I2C_bus_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
     BNO055_iERROR = BNO055_ERROR;
   }
 
-
-
-return (s8)BNO055_iERROR;
+  return (s8)BNO055_iERROR;
 }
 
-/*  \Brief: The API is used as I2C bus read
- *  \Return : Status of the I2C read
- *  \param dev_addr : The device address of the sensor
- *  \param reg_addr : Address of the first register,
- *  will data is going to be read
- *  \param reg_data : This data read from the sensor,
- *   which is hold in an array
- *  \param cnt : The no of byte of data to be read
- */
-s8 BNO055_I2C_bus_read(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
+s8 BNO055::BNO055_I2C_bus_read(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
 {
 
 //  int i2cReadI2CBlockData(unsigned handle, unsigned i2cReg, char *buf, unsigned count);
-    int result = i2cReadI2CBlockData(_HandleBNO, static_cast<unsigned>(reg_addr), static_cast<char *>(static_cast<void *>(reg_data)), static_cast<unsigned>(cnt));
+  int result = i2cReadI2CBlockData(m_i2c_handle, static_cast<unsigned>(reg_addr), static_cast<char *>(static_cast<void *>(reg_data)), static_cast<unsigned>(cnt));
 
-//
-//    printf("[RD] ");
-//    printf("dev_addr: 0x%02x | ", dev_addr);
-//    printf("reg_addr: 0x%02x | ", reg_addr);
-//    printf("cnt: 0x%02x | ", cnt);
-//    printf("reg_data: ");
-//
-//      for (int i = 0; i < cnt; i++) {
-//        printf("0x%02x ", static_cast<std::uint8_t>(*(reg_data + i)));
-//      }
-//
-//    printf("\n");
+  s32 BNO055_iERROR = BNO055_INIT_VALUE;
 
-    s32 BNO055_iERROR = BNO055_INIT_VALUE;
-//    u8 array[I2C_BUFFER_LEN] = { BNO055_INIT_VALUE };
-//    u8 stringpos = BNO055_INIT_VALUE;
-//
-//    array[BNO055_INIT_VALUE] = reg_addr;
+  if (result > 0) {
+    BNO055_iERROR = BNO055_SUCCESS;
+  } else {
+    BNO055_iERROR = BNO055_ERROR;
+  }
 
-    /* Please take the below API as your reference
-     * for read the data using I2C communication
-     * add your I2C read API here.
-     * "BNO055_iERROR = I2C_WRITE_READ_STRING(DEV_ADDR,
-     * ARRAY, ARRAY, 1, CNT)"
-     * BNO055_iERROR is an return value of SPI write API
-     * Please select your valid return value
-     * In the driver BNO055_SUCCESS defined as 0
-     * and FAILURE defined as -1
-     */
-
-//    for (stringpos = BNO055_INIT_VALUE; stringpos < cnt; stringpos++)
-//    {
-//        *(reg_data + stringpos) = array[stringpos];
-//    }
-
-
-    if (result > 0) {
-      BNO055_iERROR = BNO055_SUCCESS;
-    } else {
-      BNO055_iERROR = BNO055_ERROR;
-    }
-
-    return (s8)BNO055_iERROR;
+  return (s8)BNO055_iERROR;
 }
 
-/*  Brief : The delay routine
- *  \param : delay in ms
- */
-void BNO055_delay_msek(u32 msek)
+void BNO055::BNO055_delay_msek(u32 msek)
 {
-//  printf("[DL] ");
-//  printf("msek: %d\n", msek);
-
-    /*Here you can write your own delay routine*/
 
   std::this_thread::sleep_for(std::chrono::milliseconds(msek));
 //    gpioSleep(PI_TIME_RELATIVE, 0, static_cast<int>(1000*msek));
 
 }
-
-#endif
--- third_party/BNO055_driver-master/bno055_interface.h
+++ third_party/BNO055_driver-master/bno055_interface.h
@@ -1,56 +1,27 @@
 #ifndef __BNO055_INTERFACE_H__
 #define __BNO055_INTERFACE_H__
 
+#include <pigpio.h>
+
 #include "bno055.h"
 
-/*----------------------------------------------------------------------------*
-*  The following APIs are used for reading and writing of
-*   sensor data using I2C communication
-*----------------------------------------------------------------------------*/
-#ifdef  BNO055_API
-#define BNO055_I2C_BUS_WRITE_ARRAY_INDEX ((u8)1)
+class BNO055 {
 
-/*  \Brief: The API is used as I2C bus read
- *  \Return : Status of the I2C read
- *  \param dev_addr : The device address of the sensor
- *  \param reg_addr : Address of the first register,
- *   will data is going to be read
- *  \param reg_data : This data read from the sensor,
- *   which is hold in an array
- *  \param cnt : The no of byte of data to be read
- */
-s8 BNO055_I2C_bus_read(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt);
+ public:
+  BNO055();
+  ~BNO055();
 
-/*  \Brief: The API is used as SPI bus write
- *  \Return : Status of the SPI write
- *  \param dev_addr : The device address of the sensor
- *  \param reg_addr : Address of the first register,
- *   will data is going to be written
- *  \param reg_data : It is a value hold in the array,
- *  will be used for write the value into the register
- *  \param cnt : The no of byte of data to be write
- */
-s8 BNO055_I2C_bus_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt);
+  bno055_euler_double_t GetEuler();
 
-/*
- * \Brief: I2C init routine
- */
-s8 I2C_routine(void);
+ private:
+  bno055_t m_sensor_parameters;
+  static int16_t m_i2c_handle;
+  int m_i2c_channel = 1;
 
-/*  Brief : The delay routine
- *  \param : delay in ms
- */
-void BNO055_delay_msek(u32 msek);
+  static s8 BNO055_I2C_bus_read(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt);
+  static s8 BNO055_I2C_bus_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt);
+  static void BNO055_delay_msek(u32 msek);
+
+};
 
 #endif
-
-/********************End of I2C APIs declarations***********************/
-
-/* This API is an example for reading sensor data
- *  \param: None
- *  \return: communication result
- */
-s32 bno055_init(void);
-s32 bno055_suspend(void);
-
-#endif /*__BNO055_INTERFACE_H__*/
